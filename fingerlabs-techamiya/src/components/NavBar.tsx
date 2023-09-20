70cf160429a624bfc65a81d98d2467770b85d5f6
lydiacho <sheecho0804@ewhain.net>
2023-09-20 22:09:34 +0900

    design: NavBar UI

--- fingerlabs-techamiya/src/components/NavBar.tsx
+++ fingerlabs-techamiya/src/components/NavBar.tsx
@@ -1,5 +1,36 @@
+import { styled } from "styled-components";
+import TRAITS from "../data/traits";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const FILTER = Object.keys(TRAITS);
+  return (
+    <St.Wrapper>
+      {FILTER.map((el, idx) => (
+        <St.FilterItem key={idx}>
+          <p>{el}</p>
+          <St.Line />
+        </St.FilterItem>
+      ))}
+    </St.Wrapper>
+  );
 };
 
 export default NavBar;
+
+const St = {
+  Wrapper: styled.ul`
+    width: 26.4rem;
+  `,
+  FilterItem: styled.li`
+    & > p {
+      padding: 2rem 0rem;
+      font-size: 1.6rem;
+      font-weight: 600;
+    }
+  `,
+  Line: styled.div`
+    width: 100%;
+    height: 0.1rem;
+    background-color: ${({ theme }) => theme.colors.purple1};
+  `,
+};
