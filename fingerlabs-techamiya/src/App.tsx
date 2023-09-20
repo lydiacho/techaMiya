593dec404f4edb4db4c25005f4808161c1ff034b
lydiacho <sheecho0804@ewhain.net>
2023-09-20 21:16:12 +0900

    setting: GlobalStyle, theme

--- fingerlabs-techamiya/src/App.tsx
+++ fingerlabs-techamiya/src/App.tsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyle";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ fingerlabs-techamiya/src/styles/theme.ts
@@ -0,0 +1,19 @@
+import { css, DefaultTheme } from "styled-components";
+
+const colors = {
+  bg: "#180D36",
+  purple1: "#7F7991",
+  purple2: "#2A2045",
+};
+
+const fonts = {
+  title: css`
+    font-family: "Dongle", sans-serif;
+  `,
+};
+
+const theme: DefaultTheme = {
+  colors,
+  fonts,
+};
+export default theme;
