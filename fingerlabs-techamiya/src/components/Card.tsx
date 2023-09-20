73b93120e92dcaa32dda152e4d2385ba85b25e93
lydiacho <sheecho0804@ewhain.net>
2023-09-20 21:29:04 +0900

    design: Card Grid UI

---
+++ fingerlabs-techamiya/src/components/Card.tsx
@@ -0,0 +1,34 @@
+import { styled } from "styled-components";
+
+const Card = ({ name }: { name: string }) => {
+  return (
+    <St.Wrapper>
+      <St.Image />
+      <St.Name>{name}</St.Name>
+    </St.Wrapper>
+  );
+};
+
+export default Card;
+
+const St = {
+  Wrapper: styled.article`
+    display: flex;
+    flex-direction: column;
+    width: 29rem;
+    border-radius: 0.8rem;
+
+    overflow: hidden;
+  `,
+  Image: styled.img`
+    background-color: gray;
+    width: 100%;
+    height: 29rem;
+  `,
+  Name: styled.h2`
+    padding: 2rem 2.4rem;
+    background-color: ${({ theme }) => theme.colors.purple2};
+    font-size: 2rem;
+    text-align: start;
+  `,
+};
--- fingerlabs-techamiya/src/components/CardGallery.tsx
+++ fingerlabs-techamiya/src/components/CardGallery.tsx
@@ -1,5 +1,33 @@
+import { styled } from "styled-components";
+import Card from "./Card";
+
 const CardGallery = () => {
-  return <div>CardGallery</div>;
+  const DATA = [
+    "MIYA #0001",
+    "MIYA #0002",
+    "MIYA #0003",
+    "MIYA #0004",
+    "MIYA #0005",
+    "MIYA #0006",
+    "MIYA #0007",
+    "MIYA #0008",
+    "MIYA #0009",
+  ];
+  return (
+    <St.Wrapper>
+      {DATA.map((el, idx) => (
+        <Card name={el} key={idx} />
+      ))}
+    </St.Wrapper>
+  );
 };
 
 export default CardGallery;
+
+const St = {
+  Wrapper: styled.main`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 3rem;
+  `,
+};
