ccb0add539698107caaaa274b032fc8fb73d5b8b
lydiacho <sheecho0804@ewhain.net>
2023-09-21 17:21:40 +0900

    chore: 안쓰는 주석 코드 삭제

--- fingerlabs-techamiya/src/hooks/useGetMiya.ts
+++ fingerlabs-techamiya/src/hooks/useGetMiya.ts
@@ -11,39 +11,9 @@ const useGetMiya = () => {
     provider
   );
 
-  // useEffect(() => {
-  //   if (idx !== -1 && inView) {
-  //     if (idx === 999) {
-  //       miya
-  //         .tokenURI(idx)
-  //         .then(fetch)
-  //         .then((res) => res.json())
-  //         .then((json) => JSON.stringify(json, null, 2))
-  //         .then((json) => JSON.parse(json))
-  //         .then((res) => {
-  //           setData((prev) => [...prev, res]);
-  //         });
-  //       setIdx(-1);
-  //     } else {
-  //       for (let i = 0; i < 9; i++) {
-  //         miya
-  //           .tokenURI(idx + i)
-  //           .then(fetch)
-  //           .then((res) => res.json())
-  //           .then((json) => JSON.stringify(json, null, 2))
-  //           .then((json) => JSON.parse(json))
-  //           .then((res) => {
-  //             setData((prev) => [...prev, res]);
-  //           });
-  //       }
-  //       setIdx((prev) => prev + 9);
-  //     }
-  //   }
-  // }, [inView]);
-
   // 너무 느림. 최적화 필요
   useEffect(() => {
-    if (data.length < 100) {
+    if (data.length < 1000) {
       miya
         .tokenURI(data.length)
         .then(fetch)
