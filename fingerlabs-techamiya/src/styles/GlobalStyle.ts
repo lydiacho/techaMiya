418b58cf103c09bd263fb252cb87fc078da94605
lydiacho <sheecho0804@ewhain.net>
2023-09-21 12:06:50 +0900

    style: 최상단 background-color 영역 수정

--- fingerlabs-techamiya/src/styles/GlobalStyle.ts
+++ fingerlabs-techamiya/src/styles/GlobalStyle.ts
@@ -150,6 +150,8 @@ ${reset}
     /* overflow-y: auto; */
     -ms-overflow-style: none; /* 인터넷 익스플로러 */
     scrollbar-width: none; /* 파이어폭스 */
+
+    background-color: ${({ theme }) => theme.colors.bg};;
 }
 #root::-webkit-scrollbar {
     display: none; /* 크롬, 사파리, 오페라, 엣지 */
