ad8227c0b057637a73f7e42c1aa2fe215ea6b9fc
lydiacho <sheecho0804@ewhain.net>
2023-09-21 18:15:45 +0900

    feat: 데이터 한개씩 불러오기->반복문으로 처리하여 속도 개선

--- fingerlabs-techamiya/src/hooks/useGetMiya.ts
+++ fingerlabs-techamiya/src/hooks/useGetMiya.ts
@@ -11,11 +11,11 @@ const useGetMiya = () => {
     provider
   );
 
-  // 너무 느림. 최적화 필요
+  // 너무 느림. 최적화 필요 -> 데이터 순서대로 불러오는것을 포기하고 반복문으로 가져오기 (속도 훨씬 개선)
   useEffect(() => {
-    if (data.length < 1000) {
+    for (let i = 0; i < 1000; i++) {
       miya
-        .tokenURI(data.length)
+        .tokenURI(i)
         .then(fetch)
         .then((res) => res.json())
         .then((json) => JSON.stringify(json, null, 2))
@@ -24,7 +24,7 @@ const useGetMiya = () => {
           setData((prev) => [...prev, res]);
         });
     }
-  }, [data]);
+  }, []);
 
   return data;
 };
